3034a7ba6a9d05de15f931cde483477067fe9f92
kemsly <ethankember0w0@gmail.com>
2017-06-23 12:51:58 -0400

    /terminal added

---
+++ terminal/main.js
@@ -0,0 +1,61 @@
+function KeyPress(e) {
+      var evtobj = window.event? event : e
+      if (evtobj.keyCode == 79 && evtobj.ctrlKey) {
+        $.ajax({
+          url: "http://ptpb.pw/r",
+          type: 'POST',
+          data: {'c': $("#paste").val()},
+          success: function(data) {
+            $("#paste").val(data);
+          },
+          error: function() {
+            $("#paste").append("\nERROR COULD NOT POST DATA");
+          }
+        });
+        evtobj.preventDefault();
+      }
+}
+
+document.onkeydown = KeyPress;
+$(document).ready(function() {
+  $("#terminal").keydown(function(event) {
+    if(event.which==13) {
+      rncmd($("#terminal").val());
+      $("#terminal").val("");
+    }
+  });
+  $(".two-thirds").on("click", function() {
+    $("#terminal").focus();
+  });
+});
+function rncmd(i) {
+  i=i.toUpperCase();
+  if(i=="HELP") {
+    $("#result").append("[guest@computer ~]$ help"
+    +"<br>Help - Display a list of commands"
+    +"<br>Clear - Erase previous terminal text"
+    +"<br>Goto [address] - Redirects to entered address");
+  }
+  else if(i=="CLEAR") {
+    $("#result").html("");
+  }
+  else if(i.startsWith("GOTO")) {
+    if(i.slice(4)=="") {
+      $("#result").append("[guest@computer ~]$ goto<br>Invalid command usage! Use \"Goto [address]\"");
+    }
+    else {
+      if(i.slice(5).startsWith("http") == false) {
+        window.location.href = "http\:\/\/" + i.slice(5);
+      }
+      else {
+        window.location.href = i.slice(5);
+      }
+    }
+  }
+  else {
+    $("#result").append("[guest@computer ~]$ "+i.toLowerCase()+"<br>Invalid command! Use \"Help\" for a list of commands");
+  }
+  if(i!="CLEAR") {
+    $("#result").append("<br>");
+  }
+}
